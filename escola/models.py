7da6cf761ee23ecb593913f8239b9a943a93750f
leonardodiasc <leonardodiass@gmail.com>
2023-02-07 12:49:45 -0300

    first

---
+++ escola/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Aluno (models.Model):
+    nome = models.CharField(max_length=30)
+    rg = models.CharField(max_length=9, primary_key=True)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateTimeField()
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avancado')
+    )
+    codigo_curso= models.CharField(max_length=10, primary_key=True)
+    descricao=  models.CharField(max_length=100)
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null= False, default='B')
+    def __str__(self):
+        return self.descricao
+
